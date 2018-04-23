fdc4aacb592a29af51b73583e7b2f6125c25e9f3
Chris Ruiz <cruiz1773@yahoo.com>
2018-04-23 12:06:15 -0700

    Fixed crashing bug in onMapReady function

--- app/src/main/java/com/example/administrator/lazyapp/MapActivity.java
+++ app/src/main/java/com/example/administrator/lazyapp/MapActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.lazyapp;
 import android.*;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,9 +14,14 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 /**
  * Created by Chris on 3/25/2018.
@@ -27,6 +33,7 @@ public class MapActivity extends AppCompatActivity {
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSTION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15;
 
     private static final String TAG = "MapActivity";
 
@@ -43,13 +50,40 @@ public class MapActivity extends AppCompatActivity {
         getLocationPermission();
     }
 
-    private void getDeviceLocation()
-    {
-        Log.d(TAG, "getDeviceLocation: Getting the current devices location");
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: Getting the devices current location");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if (mLocationPermissionGranted) {
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: Found Location");
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+                        } else {
+                            Log.d(TAG, "onComplete: Current location is null");
+                            Toast.makeText(MapActivity.this, "Unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: " + e.getMessage());
+        }
     }
 
-    private void initMap()
-    {
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", long: " + latLng.longitude);
+        nmap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    private void initMap() {
         Log.d(TAG, "initMap: Initializing map");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
@@ -59,6 +93,24 @@ public class MapActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Map is ready", Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "onMapReady: map is ready");
                 nmap = googleMap;
+
+                if (mLocationPermissionGranted) {
+                    getDeviceLocation();
+
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    nmap.setMyLocationEnabled(true);
+                    nmap.getUiSettings().setMyLocationButtonEnabled(false);
+
+                }
             }
         });
     }
